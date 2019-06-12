b16f95b94dbd5ca5485db47364d39747594aab37
zn <zn993160060@icloud.com>
2019-06-12 11:58:12 +0800

    v 2.0.4改

--- pages/baike_next/baike_next.js
+++ pages/baike_next/baike_next.js
@@ -1,18 +1,17 @@
 // pages/baike_next/baike_next.js
 Page({
   data: {
-    arr_length: "",//数组返回长度
-    arr_random:"",//出随机数
-    food_search:"",
-    arr_search:[],//返回数组
-    arr_search_show:""//赋值一个渲染前端的对象
+    arr_length: "", //数组返回长度
+    arr_random: "", //出随机数
+    food_search: "",
+    arr_search: [], //返回数组
   },
-  onLoad:function(options){
+  onLoad: function(options) {
     var that = this;
     this.setData({
       food_search: options.food_search,
     })
-    
+
     wx.request({
       url: 'https://zn1121.com/wiki_baike',
       method: 'GET',
@@ -23,25 +22,25 @@ Page({
         food_search: this.data.food_search
       },
       dataType: 'json',
-      success: function (res) {
-        that.setData({ 
-            arr_search: res.data,
-            arr_length:res.data.length,
-            arr_random: Math.floor(Math.random() * res.data.length - 1),
-            
-          });
+      success: function(res) {
         that.setData({
-          arr_search_show: res.data[that.data.arr_random]
-        })
-        console.log("random",that.data.arr_random)
+          arr_length: res.data.length,
+          arr_random: Math.floor(Math.random() * res.data.length),
+          // arr_search: res.data[that.data.arr_random]
+        });
+        that.setData({
+          arr_search: Array(res.data[that.data.arr_random])
+        });
+        console.log("array_length", res.data.length)
+        console.log("random", that.data.arr_random)
         console.log("resdata", res.data[that.data.arr_random])
-        console.log("random_cai", that.data.arr_search_show)
-        console.log(res.data);
+        console.log("arr_search", that.data.arr_search)
+        // console.log("random_cai", that.data.arr_search_show)
       }
     })
-    
+
   },
-  
+
 
 
 
